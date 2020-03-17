efbf1097dbe956d0a96928d30b4cfe7cc3d0286d
iDaniiel <idaniiel16@gmail.com>
2020-03-16 23:57:44 -0600

    Se agrega el proyecto de email con core

---
+++ ServicioEmail/Elektra.ServicioEmail/Controllers/EnviarEmailController.cs
@@ -0,0 +1,27 @@
+﻿using Elektra.ServicioEmail.Servicios;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Elektra.ServicioEmail.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EnviarEmailController : ControllerBase
+    {
+        private readonly IEmail _emailServicio;
+        public EnviarEmailController(IEmail emailServicio)
+        {
+            _emailServicio = emailServicio;
+        }
+
+        //[HttpPost]
+        [Route("EnviarEmail/send-email")]
+        public async Task<IActionResult> EnviarEmailAsync([System.Web.Http.FromUri]string email, string subject, string message)
+        {
+            await _emailServicio.SendEmail(email, subject, message);
+            return Ok();
+        }
+
+
+    }
+}
